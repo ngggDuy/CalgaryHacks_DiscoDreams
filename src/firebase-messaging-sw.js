e55b25fb945eaf3c4049552949699349f04cc2a2
ekeskin <ekeskin@students.cs.ubc.ca>
2021-02-14 08:10:04 +0300

    meterBar version 6

---
+++ src/firebase-messaging-sw.js
@@ -0,0 +1,34 @@
+importScripts('https://www.gstatic.com/firebasejs/8.2.7/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.2.7/firebase-messaging.js');
+var firebase = require("firebase/app");
+
+// Add the Firebase products that you want to use
+require("firebase/app");
+require("firebase/messaging");
+var firebaseConfig = {
+    apiKey: "AIzaSyBR7BEXnWTScVSoJ5jzoFPfZuqb0tiZkx8",
+    authDomain: "friendshipmeter.firebaseapp.com",
+    projectId: "friendshipmeter",
+    storageBucket: "friendshipmeter.appspot.com",
+    messagingSenderId: "719040243429",
+    appId: "1:719040243429:web:4b5b111f1f7a07afca960c",
+    measurementId: "G-XVTEWPHJYL"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    // Customize notification here
+    const notificationTitle = 'Background Message Title';
+    const notificationOptions = {
+        body: 'Background Message body.'
+    };
+
+    self.registration.showNotification(notificationTitle,
+        notificationOptions).then(r => {
+            console.log("success");
+    });
+});
---
+++ src/meterBar.js
@@ -0,0 +1,194 @@
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+var firebase = require("firebase/app");
+
+// Add the Firebase products that you want to use
+require("firebase/auth");
+require("firebase/firestore");
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyBR7BEXnWTScVSoJ5jzoFPfZuqb0tiZkx8",
+    authDomain: "friendshipmeter.firebaseapp.com",
+    projectId: "friendshipmeter",
+    storageBucket: "friendshipmeter.appspot.com",
+    messagingSenderId: "719040243429",
+    appId: "1:719040243429:web:4b5b111f1f7a07afca960c",
+    measurementId: "G-XVTEWPHJYL"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const firestore = firebase.firestore();
+
+const messaging = firebase.messaging();
+
+messaging.getToken({vapidKey: "BA_qB5kfbiiHQgMnGa6HJTuTs0Cc3tghhwMjg6V1IDWoj4OXIFEV6Ce5O9uyDNlbF4lediZQdb7pOD6RVIloD30"}).then((currentToken) => {
+    if (currentToken) {
+        // Send the token to your server and update the UI if necessary
+        console.log("access granted");
+    } else {
+        // Show permission request UI
+        console.log('No registration token available. Request permission to generate one.');
+        // ...
+    }
+}).catch((err) => {
+    console.log('An error occurred while retrieving token. ', err);
+    // ...
+});
+
+messaging.onMessage(function (payLoad) {
+    // catches user and sends different notification if user already on page
+    console.log('onMessage: ' + payLoad);
+});
+
+// // for testing purposes. Comment out after finishing tests.
+// const idText = document.getElementById("idText");
+// const friendsText = document.getElementById("friendsText");
+// const metricsText = document.getElementById("metricsText");
+// const responsesText = document.getElementById("responsesText");
+// const saveButton = document.getElementById("saveButton");
+//
+// saveButton.addEventListener("click", function() {
+//     const idSave = "someUserName";
+//     const friendsSave = ["", "please", "help", ":("];
+//     const metricsSave = [1,2,3,4,5,6,7,8,9,0];
+//     const responsesSave = ["please help"];
+//
+//     let user: any = new User(idSave, friendsSave, metricsSave, responsesSave);
+//     saveUserData(user);
+// });
+
+
+
+function saveUserData(user) {
+    let path = "users/" + user.getId();
+    let docRef = firestore.doc(path);
+    docRef.set({
+        id: user.getId(),
+        friendsList: user.getFriendsList(),
+        responses: user.getResponses(),
+        scores: user.getMetrics(),
+    }).then( function() {
+        console.log("success, glorious!");
+    }).catch( function(error) {
+        console.log("error");
+    });
+}
+
+
+
+// TODO: find out how we can swap this for google authentication
+function authenticateUser(username, password) {
+    // assume this shit works.
+
+}
+
+class User {
+
+    // private name: string;
+    id; // this should be the auth's uid
+    friendsList;
+
+    metrics;
+    responses;
+
+    constructor(id, friendsList, metrics, responses) {
+        this.id = id;
+        this.friendsList = friendsList;
+        this.metrics = metrics;
+        this.responses = responses;
+    }
+
+    getId()  {
+        return this.id;
+    }
+
+    getFriendsList() {
+        return this.friendsList;
+    }
+
+    getMetrics() {
+        return this.metrics;
+    }
+
+    getResponses() {
+        return this.responses;
+    }
+}
+
+class meterBar {
+
+    point;
+    CHANGE_AMOUNT = 5;
+
+    constructor() {
+        this.point = 50;
+    }
+
+    incVal() {
+        this.point += CHANGE_AMOUNT;
+    }
+
+    decVal() {
+        this.point -= CHANGE_AMOUNT;
+    }
+
+    getCurrentVal()  {
+        return this.point;
+    }
+
+    // return "RED" or "YELLOW" or "GREEN"
+    getColor() {
+        if (this.point < 30) {
+            return "RED";
+        } else if (this.point < 60) {
+            return "YELLOW";
+        } else {
+            return "GREEN";
+        }
+    }
+}
+
+
+class Survey {
+
+    greenQns = ["How is your day going?", "Any issues you want to talk about?", "If you could live anywhere, where would it be?", "What weird food combinations do you really enjoy?"];
+    yellowQns = ["How are you feeling today?", "Anything you want to talk about?", "Any issues you want to talk about?"];
+    redQns = ["Are you alive?"];
+
+    responses; // recorded responses here, pass this to machine learning algorithm in the future
+    currentMeterBar;
+
+    constructor(bar, moreGreenQns, moreYellowQns, moreRedQns) {
+        this.currentMeterBar = bar;
+        this.greenQns = this.greenQns.concat(moreGreenQns);
+        this.yellowQns = this.greenQns.concat(moreYellowQns);
+        this.redQns = this.greenQns.concat(moreRedQns);
+    }
+
+    getSurveyQuestions() {
+        let color = this.currentMeterBar.getColor();
+
+        if (color === "GREEN") {
+            return this.greenQns;
+        } else if (color === "YELLOW") {
+            return this.yellowQns;
+        } else {
+            return this.redQns;
+        }
+    }
+
+    recordResponses(responses) {
+        this.responses = responses;
+    }
+
+}
+
+const idSave = "randomJSDude";
+const friendsSave = ["", "please", "help", ":("];
+const metricsSave = [1,2,3,4,5,6,7,8,9,0];
+const responsesSave = ["please help"];
+
+let user = new User(idSave, friendsSave, metricsSave, responsesSave);
+saveUserData(user);
