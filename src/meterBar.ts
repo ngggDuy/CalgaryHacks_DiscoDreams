49a46582c42a70ac4b3b49bbb63bc59953435220
LukeLNH <55564765+LukeLNH@users.noreply.github.com>
2021-02-14 08:07:22 +0700

    In the process of converting to js

--- src/meterBar.ts
+++ src/meterBar.ts
@@ -21,27 +21,27 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 var firestore = firebase.firestore();
 
-// for testing purposes. Comment out after finishing tests.
-const idText = document.getElementById("idText");
-const friendsText = document.getElementById("friendsText");
-const metricsText = document.getElementById("metricsText");
-const responsesText = document.getElementById("responsesText");
-const saveButton = document.getElementById("saveButton");
+// // for testing purposes. Comment out after finishing tests.
+// const idText = document.getElementById("idText");
+// const friendsText = document.getElementById("friendsText");
+// const metricsText = document.getElementById("metricsText");
+// const responsesText = document.getElementById("responsesText");
+// const saveButton = document.getElementById("saveButton");
+//
+// saveButton.addEventListener("click", function() {
+//     const idSave = "someUserName";
+//     const friendsSave = ["", "please", "help", ":("];
+//     const metricsSave = [1,2,3,4,5,6,7,8,9,0];
+//     const responsesSave = ["please help"];
+//
+//     let user: any = new User(idSave, friendsSave, metricsSave, responsesSave);
+//     saveUserData(user);
+// });
 
-saveButton.addEventListener("click", function() {
-    const idSave = "someUserName";
-    const friendsSave = ["", "please", "help", ":("];
-    const metricsSave = [1,2,3,4,5,6,7,8,9,0];
-    const responsesSave = ["please help"];
-
-    let user: any = new User(idSave, friendsSave, metricsSave, responsesSave);
-    saveUserData(user);
-});
 
 
-
-export function saveUserData(user: any) {
-    let path: string = "users/" + user.getId();
+function saveUserData(user) {
+    let path = "users/" + user.getId();
     let docRef = firestore.doc(path);
     docRef.set({
         id: user.getId(),
@@ -55,7 +55,7 @@ export function saveUserData(user: any) {
     });
 }
 
-export function getUserData(user: any) {
+function getUserData(user) {
 
 }
 
@@ -63,7 +63,7 @@ export function getUserData(user: any) {
 
 
 // TODO: find out how we can swap this for google authentication
-export function authenticateUser(username: string, password: string) {
+function authenticateUser(username, password) {
     // assume this shit works.
 
 }
@@ -71,99 +71,108 @@ export function authenticateUser(username: string, password: string) {
 class User {
 
     // private name: string;
-    private id: string; // this should be the auth's uid
-    private friendsList: string[];
+    id; // this should be the auth's uid
+    friendsList;
 
-    private metrics: number[];
-    private responses: string[];
+    metrics;
+    responses;
 
-    constructor(id: string, friendsList: string[], metrics: number[], responses: string[]) {
+    constructor(id, friendsList, metrics, responses) {
         this.id = id;
+        this.friendsList = friendsList;
         this.metrics = metrics;
         this.responses = responses;
     }
 
-    public getId():string  {
+    getId()  {
         return this.id;
     }
 
-    public getFriendsList():string[] {
+    getFriendsList() {
         return this.friendsList;
     }
 
-    public getMetrics(): number[] {
+    getMetrics() {
         return this.metrics;
     }
 
-    public getResponses(): string[] {
+    getResponses() {
         return this.responses;
     }
 }
 
-export class meterBar {
+// class meterBar {
+//
+//     point;
+//     CHANGE_AMOUNT = 5;
+//
+//     constructor() {
+//         this.point = 50;
+//     }
+//
+//     public incVal() {
+//         this.point += 5;
+//     }
+//
+//     public decVal() {
+//         this.point -= 5;
+//     }
+//
+//     public getCurrentVal()  {
+//         return this.point;
+//     }
+//
+//     // return "RED" or "YELLOW" or "GREEN"
+//     public getColor() {
+//         if (this.point < 30) {
+//             return "RED";
+//         } else if (this.point < 60) {
+//             return "YELLOW";
+//         } else {
+//             return "GREEN";
+//         }
+//     }
+// }
+//
+//
+// class Survey {
+//
+//     greenQns = ["How is your day going?", "Any issues you want to talk about?", "If you could live anywhere, where would it be?", "What weird food combinations do you really enjoy?"];
+//     yellowQns = ["How are you feeling today?", "Anything you want to talk about?", "Any issues you want to talk about?"];
+//     redQns = ["Are you alive?"];
+//
+//     responses; // recorded responses here, pass this to machine learning algorithm in the future
+//     currentMeterBar;
+//
+//     constructor(bar, moreGreenQns, moreYellowQns, moreRedQns) {
+//         this.currentMeterBar = bar;
+//         this.greenQns = this.greenQns.concat(moreGreenQns);
+//         this.yellowQns = this.greenQns.concat(moreYellowQns);
+//         this.redQns = this.greenQns.concat(moreRedQns);
+//     }
+//
+//     public getSurveyQuestions() {
+//         let color = this.currentMeterBar.getColor();
+//
+//         if (color === "GREEN") {
+//             return this.greenQns;
+//         } else if (color === "YELLOW") {
+//             return this.yellowQns;
+//         } else {
+//             return this.redQns;
+//         }
+//     }
+//
+//     public recordResponses(responses: string[]) {
+//         this.responses = responses;
+//     }
+//
+// }
 
-    private point: number;
-    private CHANGE_AMOUNT: number = 5;
+const idSave = "someUserName";
+const friendsSave = ["", "please", "help", ":("];
+const metricsSave = [1,2,3,4,5,6,7,8,9,0];
+const responsesSave = ["please help"];
 
-    constructor() {
-        this.point = 50;
-    }
-
-    public incVal() {
-        this.point += 5;
-    }
-
-    public decVal() {
-        this.point -= 5;
-    }
-
-    public getCurrentVal():number  {
-        return this.point;
-    }
-
-    // return "RED" or "YELLOW" or "GREEN"
-    public getColor(): string {
-        if (this.point < 30) {
-            return "RED";
-        } else if (this.point < 60) {
-            return "YELLOW";
-        } else {
-            return "GREEN";
-        }
-    }
-}
-
-
-export class Survey {
-
-    private greenQns: string[] = ["How is your day going?", "Any issues you want to talk about?", "If you could live anywhere, where would it be?", "What weird food combinations do you really enjoy?"];
-    private yellowQns: string[] = ["How are you feeling today?", "Anything you want to talk about?", "Any issues you want to talk about?"];
-    private redQns: string[] = ["Are you alive?"];
-
-    private responses: string[]; // recorded responses here, pass this to machine learning algorithm in the future
-    private currentMeterBar: any;
-
-    constructor(bar: any, moreGreenQns: string[], moreYellowQns: string[], moreRedQns: string[]) {
-        this.currentMeterBar = bar;
-        this.greenQns = this.greenQns.concat(moreGreenQns);
-        this.yellowQns = this.greenQns.concat(moreYellowQns);
-        this.redQns = this.greenQns.concat(moreRedQns);
-    }
-
-    public getSurveyQuestions() {
-        let color: string = this.currentMeterBar.getColor();
-
-        if (color === "GREEN") {
-            return this.greenQns;
-        } else if (color === "YELLOW") {
-            return this.yellowQns;
-        } else {
-            return this.redQns;
-        }
-    }
-
-    public recordResponses(responses: string[]) {
-        this.responses = responses;
-    }
-
-}
+let user = new User(idSave, friendsSave, metricsSave, responsesSave);
+saveUserData(user);
