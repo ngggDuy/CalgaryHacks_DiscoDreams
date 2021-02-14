9226aaf98e854c19765c5886155031fae8fcd3d2
ekeskin <ekeskin@students.cs.ubc.ca>
2021-02-14 20:01:17 +0300

    index.js implementation commented out currently, working UI

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();/*
 // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require("firebase-functions");
 
@@ -10,6 +28,7 @@ exports.trimResults = functions.firestore
     .document("users/{email}")
     .oncreate((snapshot, context) => {
         const data = snapshot.data();
+        /*
         if (data) {
             if (data.scores.length > 7) { // limit to 7 elements
                 let newScores = data.scores;
@@ -20,7 +39,8 @@ exports.trimResults = functions.firestore
                 }
                 snapshot.ref.update({scores: newScores});
             }
-
+            */
+ /*
             // if (data.scores.responses > 7) { // im not sure this is even needed. Comment out if redundant
             //     let newResponses = data.responses;
             //     let num = data.responses.length - 7;
@@ -33,3 +53,5 @@ exports.trimResults = functions.firestore
             // }
         }
     }).catch(function(error) {console.log(error)});
+
+ */
